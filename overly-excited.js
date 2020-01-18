08f87f51a1a2c65d8105e61a5ca3cd0b03676441
Bito Mann <bito@bitomann.com>
2020-01-18 12:32:31 -0600

    exclamation

--- overly-excited.js
+++ overly-excited.js
@@ -9,19 +9,20 @@ let sentence = ["The","walrus","danced","through","the","trees","in","the","ligh
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement (theWordArray) {
+function addExcitement (theWordArray, punctuation) {
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = []
 // vvv grabs theWordArray argument in the addExcitement function, iterates over vvvvvvvvvvvvvvvvv
     for (let i = 0; i < theWordArray.length; i++) {
         // Concatenate the new word onto buildMeUp
-if (buildMeUp.length + 1) {
-    buildMeUp.push(`${theWordArray[i]}`)
+if ((buildMeUp.length + 1) % 3 === 0){
+    buildMeUp.push(`${theWordArray[i]} ${punctuation}`);
     console.log(buildMeUp);
 }
+else buildMeUp.push(`${theWordArray[i]}`);
 // Print buildMeUp to the console
-
-}}
+}
+}
 
 // Invoke the function and pass in the array
-addExcitement(sentence)
+addExcitement(sentence, '!')
